8f4734c5b06aaf0b79471dc4f7e8d9e9d4fcab59
Andrea Sousa <77935458+andreadcsousa@users.noreply.github.com>
2021-04-28 18:39:25 -0300

    commit to main

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 function ativaScrollSuave(selector) {
+  $(selector).click(function (event) {
+    event.preventDefault();
+    var target = $(this).attr("href");
 
-    $(selector).click(function(event) {
-        
-        event.preventDefault();
-        var target = $(this).attr('href');
-        
-        $('html, body').animate({
-            scrollTop: $(target).offset().top
-        }, 500)
-    });
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top,
+      },
+      500
+    );
+  });
 }
 
-ativaScrollSuave('a[href*=panel-about]');
-ativaScrollSuave('a[href*=panel-speakers]');
-ativaScrollSuave('a[href*=panel-form]');
+ativaScrollSuave("a[href*=secao-chamada]");
+ativaScrollSuave("a[href*=secao-palestrantes]");
+ativaScrollSuave("a[href*=secao-formulario]");
